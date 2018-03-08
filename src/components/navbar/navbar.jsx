1e2febafd62340bf087956249d3e6f50b65c5c88
Daniel Chong <danchong625@gmail.com>
2018-03-07 23:49:55 -0800

    [style] adds spacing to nav links

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,6 +6,8 @@ import PropTypes from 'prop-types';
 
 import { authLogout } from '../../actions/authActions';
 
+import './navbar.css';
+
 class Navbar extends Component {
   constructor(props) {
     super(props);
